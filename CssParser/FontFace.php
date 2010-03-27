315ee72e84f73eb3b0d7ef7a2e1ba761585b39cc
zaininnari <zaininnari@gmail.com>
2010-03-27 19:39:02 +0900

    CssParser/FontFace.phpの追加

---
+++ CssParser/FontFace.php
@@ -0,0 +1,21 @@
+<?php
+
+class CssParser_FontFace extends CssParser_RuleSet
+{
+	protected $type = '@font-face';
+
+	/**
+	 * パースに失敗した場合はPEG_Failureを返すこと。
+	 * 成功した場合はなんらかの値を返すこと。
+	 *
+	 * @param PEG_IContext $context PEG_IContext
+	 *
+	 * @see PEG/PEG_IParser#parse($c)
+	 *
+	 * @return mixed
+	 */
+	public function parse(PEG_IContext $context)
+	{
+		return $this->parser->parse($context);
+	}
+}
